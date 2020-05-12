e86e1d63cf418b2f029b5dfe1a9c836a8dbc6737
nick-echevarria <nick.echev@gmail.com>
2020-05-12 10:07:43 -0400

    Working fetch

--- src/App.js
+++ src/App.js
@@ -6,24 +6,36 @@ let API_ENDPOINT = `http://localhost:6001/songs`
 
 class App extends React.Component {
   state = {
-
+    songs:[]
   }
   
+  getSongs = () => { 
+    fetch(API_ENDPOINT)
+    .then(resp => resp.json())
+    .then(songArray => {
+      console.log(songArray)
+        this.setState({...this.state, songs:songArray })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  } 
+
   renderNav = () => {
     return (
       <div className="simple-flex-row">
-        <button onClick={null /* TODO: Put your method to fetch the songs */}>Get Songs</button> 
-        <h1>S-not-ify 🐽</h1>
+        <button onClick={this.getSongs}>Get Songs</button> 
+        <span>S-not-ify</span>
         <input placeholder="Search by title or artist..."/>
       </div>
-    )
+    ) 
   }
 
   render(){
     return (
       <div className="App">
         {this.renderNav()} {/** The renderNav method renders a div holding the button to get songs and the title */}
-        <MainContainer /> {/** TODO: What props do I need? */}
+        <MainContainer  /> {/** TODO: What props do I need? */}
       </div>
     );
   }
