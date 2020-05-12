5d9c78873481f5e87e699dd05d762f9a053b1b13
duruchima <duruchima1@gmail.com>
2020-05-12 10:28:07 -0400

    pair programming with johnson

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Filter from './Filter';
+import SongItem from './SongItem'
 
 const SongList = props => {
     return (
@@ -19,6 +20,9 @@ const SongList = props => {
                     </thead>
                     <tbody>
                         {/** TODO: Render a SongItem component per each song here*/}
+                        {props.songs.map((song,index)=> {
+                            return <SongItem key={index} title={song.title} artist={song.artist} url={song.url} genre={song.genre} favorite={song.favorite} />
+                            })}
                     </tbody>
             </table>
         </div>
