f0faf0b169c863b84c9d6e47a6c4a94690ad965d
stephenverdi95 <stephenverdi152@gmail.com>
2020-05-12 09:43:05 -0400

    tried first 2 deliverables

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Filter from './Filter';
+import { render } from '@testing-library/react';
 
 const SongList = props => {
+    fetchFunction = () => {
+    fetch(`http://localhost:6001/songs`)
+        .then(response => response.json())
+        .then(data => renderData(data))
+    }
+    renderData = (data) => {
+        data.forEach(song => <SongItem title={song.title} artist={song.artist}/>)
+    }
+    //render() {
     return (
         <div className="half songlist">
             <h2>Song List</h2>
@@ -18,11 +28,14 @@ const SongList = props => {
                         </tr>
                     </thead>
                     <tbody>
-                        {/** TODO: Render a SongItem component per each song here*/}
+                        {
+                        fetchFunction()
+                        }
                     </tbody>
             </table>
         </div>
     )
+//}
 }
 
 export default SongList;
