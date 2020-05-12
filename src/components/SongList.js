032ba2388f24b598e4498efffce51f4f64df2d4f
Samanthaponce5 <samanthaponce5@gmail.com>
2020-05-12 10:00:44 -0400

    almost-done

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Filter from './Filter';
+import SongItem from './SongItem';
+
 
 const SongList = props => {
     return (
@@ -18,7 +20,7 @@ const SongList = props => {
                         </tr>
                     </thead>
                     <tbody>
-                        {/** TODO: Render a SongItem component per each song here*/}
+                    {props.songs.map((song,index) => <SongItem updateSong={props.updateSong}{...song} key= {index} />)}
                     </tbody>
             </table>
         </div>
