007b18d5e668ac8cfb317aad392c3f502083706c
JohnsonNYC <jkow95@gmail.com>
2020-05-12 10:24:48 -0400

    Pair with Duncan

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 const SongItem = props => {
+    // constructor(){
+    //     super();
+
+    //     this.state = {   DOES THIS NOT WORK BECUASE IT'S NOT A CLASS
+    //         Likes: 0;
+    //     };
+    // }
+
+
+    handleLike = () => {
+        this.setState(previousState => {
+            return {
+                Likes: previousState.Likes + 1
+            }
+        })
+    }
     return (
         <tr>
             <td>{props.title}</td>
@@ -8,7 +24,7 @@ const SongItem = props => {
             <td><button onClick={null /* Put your click handler here */}>Play Now</button></td>
             <td><button onClick={null /* Put your click handler here */}>Add to Queue</button></td>
             <td onClick={null /* Put your click handler here */}>{props.favorite ? "💚" : "♡"}</td>
-            <td>Likes: 0</td>
+            <td onClick={this.handleLike}>Likes: {this.state.Likes}</td>
         </tr>
     )
 }
